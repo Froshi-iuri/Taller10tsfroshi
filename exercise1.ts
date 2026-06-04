f46a3112a7fc1f9aa894458916959ed3dd29f5f1
Froshi <kg5892868@gmail.com>
2026-06-04 17:55:46 -0500

    feat: create 10 exercises empty

---
+++ exercise1.ts
@@ -0,0 +1,16 @@
+interface Estudiante{
+    id: number,
+    nombre: string,
+    programa: string,
+    promedio: number,
+}
+
+const estudiante = [
+    { id: 1, nombre: "Froshi Iuri", edad: 20, programa: "Ingeniería de Sistemas", promedio: 4.2 },
+    { id: 2, nombre: "Clerix Riul", edad: 22, programa: "Medicina", promedio: 4.8 },
+    { id: 3, nombre: "Pixmn ter", edad: 19, programa: "Derecho", promedio: 3.9 },
+    { id: 4, nombre: "terilo lores", edad: 23, programa: "Administración", promedio: 4.5 },
+    { id: 5, nombre: "larix xentor", edad: 21, programa: "Diseño Gráfico", promedio: 4.0 }
+]
+
+console.log(estudiante);
